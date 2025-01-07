6d505a0e91d743e8caf6c169045910801a4a31cd
waeltestingcode <showmaker2112@gmail.com>
2025-01-07 17:41:38 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -119,7 +119,7 @@ function App() {
 
       // Get top tracks
       const tracksResponse = await fetch(
-        'https://api.spotify.com/v1/me/top/tracks?limit=10&time_range=medium_term',
+        'https://api.spotify.com/v1/me/top/tracks?limit=10&time_range=long_term',
         {
           headers: { 'Authorization': `Bearer ${token}` }
         }
